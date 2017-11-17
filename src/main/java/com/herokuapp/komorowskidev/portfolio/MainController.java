db811e7c836a37eebdcfefc7f099d705b3266542
Krzysztof Świerkosz-Komorowski <komorowskidev@gmail.com>
2017-11-17 23:33:36 +0100

    ukończona podstawowa wersja strony

--- src/main/java/com/herokuapp/komorowskidev/portfolio/MainController.java
+++ src/main/java/com/herokuapp/komorowskidev/portfolio/MainController.java
@@ -9,9 +9,24 @@ public class MainController {
 	
     @RequestMapping("/")
     public String index(Model model) {
-    	String name = "test";
-    	model.addAttribute("name", name);
+//    	String name = "test";
+//    	model.addAttribute("name", name);
         return "index";
+    }
+    
+    @RequestMapping("/psychoterapia")
+    public String psychoterapy() {
+    	return "psychoterapy";
+    }
+    
+    @RequestMapping("/programowanie")
+    public String programming() {
+    	return "programming";
+    }
+    
+    @RequestMapping("/pasieka")
+    public String beekeeping() {
+    	return "beekeeping";
     }
 
 }
